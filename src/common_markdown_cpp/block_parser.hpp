f5d6940a86ba4fe4f9b528c478fd52f8e1f0d6dd
goto40 <pierre.bayerl@googlemail.com>
2024-09-02 22:45:35 +0200

    initial simple base for a block parser

---
+++ src/common_markdown_cpp/block_parser.hpp
@@ -0,0 +1,102 @@
+#pragma once
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+constexpr int DOCUMENT = 0;
+constexpr int LIST_ITEM = 1;
+constexpr int LIST = 2;
+constexpr int QUOTE = 3;
+constexpr int PARAGRAPH = 10;
+constexpr int CODE = 11;
+
+struct Block {
+  int block_type = DOCUMENT;
+  bool closed = false;
+  std::vector<std::shared_ptr<Block>> blocks = {};
+  std::vector<std::string> text = {};
+};
+
+inline bool is_container(Block &block) {
+  if (block.block_type < 10)
+    return true;
+  else
+    return false;
+}
+
+inline bool is_block_terminated(Block &block, std::string line) {
+  // TODO if CODE:..
+  return line.empty(); // not good enough, compare with "whitespaces or empty"
+}
+
+struct ContinuedBlockResult {
+  std::shared_ptr<Block> block_to_insert = {};
+  std::shared_ptr<Block> last_block = {};
+  std::string line;
+};
+
+inline std::shared_ptr<Block> find_last_block(std::shared_ptr<Block> block) {
+  if (block->blocks.empty())
+    return block;
+  else
+    return find_last_block(block->blocks.back());
+}
+
+inline ContinuedBlockResult
+detect_next_block_to_insert(std::shared_ptr<Block> block, std::string line) {
+  ContinuedBlockResult result = {};
+  if (block->closed)
+    return {};
+  if (is_container(*block)) {
+    if (!block->blocks.empty()) {
+      if (block->block_type == DOCUMENT) {
+        result = detect_next_block_to_insert(block->blocks.back(), line);
+      } else {
+        // TODO list, ... etc.
+      }
+    }
+    if (result.block_to_insert == nullptr)
+      result = {block, find_last_block(block), line};
+  } else { // leaf block
+    if (block->block_type == PARAGRAPH) {
+      result = {block, find_last_block(block), line};
+    } else if (block->block_type == CODE) {
+      if (line.starts_with("    ")) {
+        result = {block, find_last_block(block), line.substr(4)};
+      }
+    }
+  }
+  return result;
+}
+
+inline void add_next_line(std::shared_ptr<Block> block, std::string line) {
+  ContinuedBlockResult c = detect_next_block_to_insert(block, line);
+  if (c.block_to_insert == nullptr)
+    throw std::runtime_error("unexpected: no place to insert");
+  if (is_container(*c.block_to_insert)) {
+    // start a new block
+
+    // TODO if...
+    std::shared_ptr<Block> new_block = std::make_shared<Block>();
+    new_block->block_type = PARAGRAPH;
+    new_block->text.push_back(c.line);
+    c.block_to_insert->blocks.push_back(new_block);
+  } else {
+    // TODO if "finished"
+    if (is_block_terminated(*c.block_to_insert, c.line)) {
+      c.block_to_insert->closed = true;
+    } else {
+      c.block_to_insert->text.push_back(c.line);
+    }
+  }
+}
+
+inline void print(std::ostream &out, std::shared_ptr<Block> block,
+                  int indent = 0) {
+  out << std::string(indent, ' ') << "BLOCK:" << block->block_type << std::endl;
+  for (std::shared_ptr<Block> b : block->blocks) {
+    print(out, b, indent + 2);
+  }
+}
--- test/hello.cpp
+++ test/hello.cpp
@@ -1,9 +1,43 @@
+#include "common_markdown_cpp/block_parser.hpp"
 #include "common_markdown_cpp/version.h"
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
 
 TEST_CASE("hello_world", "[hello]") {
   std::cout << "common_markdown_cpp " << common_markdown_cpp::VERSION << " "
             << common_markdown_cpp::VERSION_SHORT << " unittests\n";
   REQUIRE(true);
+}
+
+TEST_CASE("paragraphs", "[simple parser tests]") {
+  using namespace common_markdown_cpp;
+
+  std::string text = R"(Hello
+  This is Paragraph1
+  ...
+
+  And this is Para2
+  with two lines
+
+  And this Para3
+
+  and this P4)";
+
+  std::istringstream in{text};
+  std::string line;
+  std::shared_ptr<Block> doc = std::make_shared<Block>();
+  while (std::getline(in, line)) {
+    add_next_line(doc, line);
+  }
+
+  print(std::cout, doc);
+
+  REQUIRE(doc->blocks.size() == 4);
+  CHECK(doc->blocks.at(0)->text.size() == 3);
+  CHECK(doc->blocks.at(1)->text.size() == 2);
+  CHECK(doc->blocks.at(2)->text.size() == 1);
+  CHECK(doc->blocks.at(3)->text.size() == 1);
 }
